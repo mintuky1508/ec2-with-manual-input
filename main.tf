e47889d5f3fb7bedff89e65259722de5946b3e62
mintuky1508 <mintuky1508@gmail.com>
2024-12-07 15:30:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,6 @@ resource "aws_instance" "my_ec2" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.vpc_security_group_id] # Wrap the single ID in a list
-
-  key_name = "cicd"
-
   tags = {
     Name = "MyTerraformEC2"
   }
